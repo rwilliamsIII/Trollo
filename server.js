b909a3694bc7f6aca7ef0dbc2ab22f9b55cdad23
Robert Williams <robert@a3marketing.com>
2020-07-02 20:26:32 -0500

    created server file and connected to db

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const mysql = require("mysql")
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "bear",
+    database: "trollo_db"
+  });
+
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/public"));
+}
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
+
+
+app.listen(PORT, function(){
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+})
