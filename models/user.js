7a3273d4b293b63d5cbc068ad5695f3ac9ea5487
Robert Williams <robert@a3marketing.com>
2020-07-02 20:31:57 -0500

    added user/index files in models folder

---
+++ models/user.js
@@ -0,0 +1,28 @@
+module.exports = function (sequelize, Sequelize) {
+    var User = sequelize.define('user', {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
+        firstName: {
+            type: Sequelize.STRING,
+            notEmpty: true
+        },
+        lastName: {
+            type: Sequelize.STRING,
+            notEmpty: true
+        },
+        email: {
+            type: Sequelize.STRING,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+    });
+    return User;
+}
