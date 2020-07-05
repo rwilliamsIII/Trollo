ab76abfe69292b254ebe4eff49441b7f314f04d8
kcenters <kristincenters@gmail.com>
2020-07-04 20:01:41 -0500

    created users model and route, tweaked server file

--- server.js
+++ server.js
@@ -1,34 +1,44 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const db = require("./models");
-const mongoose = require("mongoose");
+const db = require('./models');
+const mongoose = require('mongoose');
 
+app.get('/', (req, res) => res.send('API Running'));
 
-
-
-
+// Init Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/public'));
 }
-
+// Connect Database
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/trollotodos",
-    { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
-  );
-
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
-});
-
-app.use(require("./routes/api-routes"))
+	process.env.MONGODB_URI || 'mongodb://localhost:27017/trollo',
+	{
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+		useCreateIndex: true,
+	}
+);
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, './client/public/index.html'));
+	});
+}
+// Define Routes
+app.use('/users', require('./routes/users'));
 // require("./routes/html-routes.js")(app);
 
-    app.listen(PORT, function() {
-      console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-    });
+app.listen(PORT, function () {
+	console.log(
+		'==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
+		PORT,
+		PORT
+	);
+});
