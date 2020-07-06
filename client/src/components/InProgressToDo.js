afde71d38baf29d9d1322c193d4f93dfe557d1f0
Robert Williams <robert@a3marketing.com>
2020-07-06 11:57:43 -0500

    created basic structure of todos and in progress todos

---
+++ client/src/components/InProgressToDo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function InProgressToDo (props) {
+    return (
+        <div className=''></div>
+    )
+}
+
+export default InProgressToDo;
---
+++ client/src/components/ThingsToDo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ThingsToDo (props) {
+    return (
+        <div className=''></div>
+    )
+}
+
+export default ThingsToDo;
