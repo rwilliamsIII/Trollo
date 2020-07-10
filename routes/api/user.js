f3a8772aaab48dd5987fe5c1f4482f69b1a42fc9
kcenters <kristincenters@gmail.com>
2020-07-09 19:28:34 -0500

    added user login route

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,7 +1,7 @@
 module.exports = function (app) {
 	const bcrypt = require('bcryptjs');
 	const jwt = require('jsonwebtoken');
-	const passport = require('passport');
+	// const passport = require('passport');
 	const keys = require('../../config/keys');
 	const User = require('../../models/User');
 
@@ -52,83 +52,53 @@ module.exports = function (app) {
 				});
 			}
 		});
-
-		app.post('/api/user/login/', (req, res) => {
-			const { email, password } = req.body;
-
-			User.findOne({ where: { email } }).then((user) => {
-				if (!user) {
-					return res.status(404).json({ user: 'User not found' });
-				}
-				let currentUser = user.get();
-
-				//compare passwords
-				bcrypt.compare(password, currentUser.password).then((isMatch) => {
-					if (isMatch) {
-						User.findOne({
-							where: { id: user.id },
-						})
-							.then((user) => {
-								const payload = {
-									id: user.id,
-									email: user.email,
-									name: user.name,
-								};
-
-								jwt.sign(
-									payload,
-									keys.secretOrKey,
-									{ expiresIn: 3600 * 12 },
-									(err, token) => {
-										res.json({
-											...payload,
-											success: true,
-											token: `Bearer ${token}`,
-										});
-									}
-								);
-							})
-							.catch((err) => console.log(error));
-					} else {
-						return res.status(400).json({
-							password: 'User password could not be validated',
-						});
-					}
-				});
-			});
-		});
 	});
 
-	app.put(
-		'api/user/',
-		passport.authenticate('jwt', { session: false }),
-		(req, res) => {
-			console.log(req.user);
-			User.update(
-				{
-					name: req.body.name,
-					email: req.body.email,
-				},
-				{
-					where: {
-						id: req.user.id,
-					},
-				}
-			).then(() => {
-				User.findById(req.user.id)
-					.then((user) => {
-						res.status(200),
-							json({
-								email: user.email,
-								name: user.name,
-								message: 'User updated',
-								userUpdated: true,
-							});
-					})
-					.catch((err) => {
-						res.status(500).json(err);
-					});
-			});
-		}
-	);
+	app.post('/api/user/login', async (req, res) => {
+		const { email, password } = req.body;
+		const userExists = await User.findOne({ email });
+		if (!userExists) return res.status(400).send('Email/password incorrect.');
+		const match = await bcrypt.compare(password, userExists.password);
+		if (!match) return res.status(400).send('Email/password incorrect.');
+		const payload = {
+			id: userExists._id,
+			email: userExists.email,
+		};
+		const token = await jwt.sign(payload, keys.secretOrKey, {
+			expiresIn: '1hr',
+		});
+		res.status(200).send({ token: `Bearer ${token}` });
+	});
 };
+// app.put(
+// 	'api/user/',
+// 	passport.authenticate('jwt', { session: false }),
+// 	(req, res) => {
+// 		console.log(req.user);
+// 		User.update(
+// 			{
+// 				name: req.body.name,
+// 				email: req.body.email,
+// 			},
+// 			{
+// 				where: {
+// 					id: req.user.id,
+// 				},
+// 			}
+// 		).then(() => {
+// 			User.findById(req.user.id)
+// 				.then((user) => {
+// 					res.status(200),
+// 						json({
+// 							email: user.email,
+// 							name: user.name,
+// 							message: 'User updated',
+// 							userUpdated: true,
+// 						});
+// 				})
+// 				.catch((err) => {
+// 					res.status(500).json(err);
+// 				});
+// 		});
+// 	}
+// );
