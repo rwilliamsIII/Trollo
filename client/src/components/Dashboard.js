1a328947a0afcd66c37e5a5d8d535d3918108058
kcenters <kristincenters@gmail.com>
2020-07-10 19:54:47 -0500

    added login dashboard nomatch pages and node client modules

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dashbaord extends Component {
+	render() {
+		return (
+			<div className='Dashboard'>
+				<h1>Dashboard</h1>
+			</div>
+		);
+	}
+}
+
+export default Dashboard;
