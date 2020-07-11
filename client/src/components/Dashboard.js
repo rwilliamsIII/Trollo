95e8fa98296df74200dea36ab7d751e6608bc732
kcenters <kristincenters@gmail.com>
2020-07-10 22:05:14 -0500

    added dashboard starter code, added login axios call

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 class Dashboard extends Component {
+	state = {
+		redirect: false,
+	};
+
+	// componentDidMount() {
+	// 	axios.get('/api/user')
+	// 	.then(response => {
+	// 		 console.log(response);
+	// 	})
+	// 	.catch(err => console.log(err.response));
+	// 	}
+	// }
+
+	handleLogout = () => {
+		localStorage.removeItem('example-app');
+		this.setState({
+			redirect: true,
+		});
+	};
+
 	render() {
+		const { redirect } = this.state;
+		if (redirect) {
+			return <Redirect to='/' />;
+		}
 		return (
 			<div className='Dashboard'>
+				<div className='ui container'></div>
 				<h1>Dashboard</h1>
+				<p>Welcome, name</p>
+				<p>Email Address:</p>
+				<div className='ui teal button'>Logout</div>
 			</div>
 		);
 	}
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,76 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 class Login extends Component {
+	constructor() {
+		super();
+		this.state = {
+			redirect: false,
+			email: '',
+			password: '',
+			errors: {},
+		};
+	}
+	onChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	onSubmit = (e) => {
+		e.preventDefault();
+
+		const newUser = {
+			email: this.state.email,
+			password: this.state.password,
+		};
+		console.log(newUser);
+		axios
+			.post('/api/user/login', newUser)
+			.then((res) => {
+				this.setState({
+					redirect: true,
+					errors: {},
+				});
+				console.log(res.data);
+			})
+			.catch((err) => console.log(err.response.data));
+		//this.state({
+		//errors: err.response.data
+		//});
+	};
+
 	render() {
+		const { errors, redirect } = this.state;
+
+		if (redirect) {
+			return <Redirect to='/dashboard' />;
+		}
 		return (
 			<div className='Login'>
-				<h1>Login</h1>
+				<div className='ui container'>
+					<h3>Login Page </h3>
+					<form onSubmit={this.onSubmit}>
+						<div className='ui form'>
+							<input
+								type='email'
+								name='email'
+								placeholder='Email address'
+								value={this.state.email}
+								onChange={this.onChange}
+							/>
+							<input
+								type='password'
+								name='pasword'
+								placeholder='Enter Password'
+								value={this.state.password}
+								onChange={this.onChange}
+							/>
+							<div className='ui teal button'>Submit</div>
+						</div>
+					</form>
+				</div>
 			</div>
 		);
 	}
