2d8db226fc6d93ca00ce9777f55142acc70929a8
Tro <noe.monsivais@gmail.com>
2020-07-11 10:48:39 -0500

    created vanilla function called jumbotrontitle.

---
+++ client/src/components/Jumbotron.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container'
+
+function JumbotronTitle() {
+    return (
+        <div>
+
+
+        </div>
+    )
+}
+
+
+export default JumbotronTitle;
