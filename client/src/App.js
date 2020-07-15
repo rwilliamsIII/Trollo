eb4076e616400ace08dd5822dcee8278e4de3dbc
kcenters <kristincenters@gmail.com>
2020-07-14 21:02:35 -0500

    added registration page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import PrivateRoute from './utils/PrivateRoute';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import NoMatch from './pages/NoMatch';
 
@@ -12,6 +13,7 @@ function App() {
 			<Router>
 				<Switch>
 					<Route exact path='/' component={Login} />
+					<Route exact path='/register' component={Register} />
 					<PrivateRoute exact path='/dashboard' component={Dashboard} />
 					<Route component={NoMatch} />
 				</Switch>
