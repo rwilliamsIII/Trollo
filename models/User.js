515fb43d836348d7636c81eeb0cd7c62ee978225
Robert Williams <robert@a3marketing.com>
2020-07-14 22:05:50 -0500

    changed user/todo models and routes

--- models/User.js
+++ models/User.js
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+	todos: {
+		type: Array,
+		default: []
+	}
 });
 
 module.exports = mongoose.model('User', UserSchema);
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,12 +1,16 @@
 // Requiring our Todo model
 module.exports = (app) => {
     const Trollo = require('../models/trollo');
+    const User = require('../models/User');
+    const passport = require('passport');
 
 // Routes
 
 
 // Routes to todos
-
+app.get('/api/test', passport.authenticate('jwt', {session: false}), (req, res) => {
+    res.json({msg: 'It Works!'});
+})
 app.get('/api/trollos', (req, res) => {
     Trollo.find({inProgress: 'false',completed: 'false'})
         .sort({date: -1})
@@ -28,17 +32,16 @@ app.get('/api/completed', (req, res) => {
         .catch((err) => res.status(422).json(err));
 });
 
-app.post('/api/trollos',  ({ body }, res) => {
-    Trollo.create(body)
-    .then(dbTrollo => {
-    console.log('Todo Created!');
-    res.json(dbTrollo);
-    })
-    .catch(err => {
-        res.status(422).json(err);
-    });
+app.post('/api/trollos', passport.authenticate('jwt', {session: false}),  (req, res) => {
+    console.log(req.user);
+    User.updateOne({_id: req.user._id}, {$push: {todos: {
+    title: req.body.title,
+	description: req.body.description,
+	dueDate: req.body.dueDate,
+    inProgress: false,
+    completed: false }}}).then(updated => res.json({msg: 'User successfully updated'})).catch(error => res.json({err: error}));
 });
-
+// db.people.update({name: "John"}, {$push: {friends: {firstName: "Harry", lastName: "Potter"}}});
 app.delete('api/trollos/id:', (req, res) => {
     Trollo.findOneAndRemove({_id: req.body.id}, function(err, result){
         if (err) {
