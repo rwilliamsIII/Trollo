0a6632ede94c7ef40551b49a24e3b9c697182509
Tro <noe.monsivais@gmail.com>
2020-07-18 11:33:18 -0500

    Added timer code to timerCode.js.

--- client/src/components/TimerCode.js
+++ client/src/components/TimerCode.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
 
-export class TimerCode extends Component {
+class TimerCode extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			count: 0
+		}
+	}
+
 	render() {
+		const { count } = this.state
 		return (
 			<div>
-
+				<h1> Current Count: {count} </h1>
 			</div>
 		)
+	}
+	//setInterval
+	//clearInterval
+	componentDidMount() {
+		this.myInterval = setInterval(() => {
+			this.setState(prevState => ({
+				count: prevState.count + 1
+			}))
+		}, 1000)
+	}
+	componentWillUnmount() {
+		clearInterval(this.myInterval)
 	}
 }
 
